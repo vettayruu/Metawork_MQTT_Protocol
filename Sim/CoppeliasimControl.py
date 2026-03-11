c3ebcfbc662e37101a02315bc45199832b8e89f6
LIU SONGTAO <135619114+vettayruu@users.noreply.github.com>
2026-03-11 09:00:32 +0900

    Update CoppeliasimControl.py

--- Sim/CoppeliasimControl.py
+++ Sim/CoppeliasimControl.py
@@ -7,6 +7,7 @@ class CoppeliasimControl:
         self.joint_handles = [self.sim.getObject(joint_name) for joint_name in joint_name_list]
         self.tool_handles = [self.sim.getObject(tool_name) for tool_name in tool_name_list]
 
+    """ Send Target Joint Positions（rad）"""
     def send_joint_position(self, thetaBody):
         for i, handle in enumerate(self.joint_handles):
             self.sim.setJointTargetPosition(handle, thetaBody[i])
@@ -15,13 +16,13 @@ class CoppeliasimControl:
         for i, handle in enumerate(self.tool_handles):
             self.sim.setJointTargetPosition(handle, thetaTool[i])
 
+    """ Get Current Joint Positions（rad）"""
     def get_joint_position(self):
-        """读取当前的关节角（单位：弧度）"""
         joint_position = [self.sim.getJointPosition(handle) for handle in self.joint_handles]
         return joint_position
 
     def get_tool_position(self):
-        """读取当前的关节角（单位：弧度）"""
         tool_position = [self.sim.getJointPosition(handle) for handle in self.tool_handles]
         return tool_position
+
 
